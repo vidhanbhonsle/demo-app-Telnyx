9c6b0b59f05c5d7e8638a112e7ebdc51418de80a
Vidhan Bhonsle <vidhanbhonsle@gmail.com>
2021-10-05 03:16:21 +0530

    Update receive.py

--- receive.py
+++ receive.py
@@ -3,14 +3,14 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhooks', methods=['POST'])
 def webhooks():
-    if request.method == 'POST':
-        body = request.json
-        print(body)
-        return 'success', 200
-    else:
-        abort(400)
-
+    body = request.json
+    print(body)
+    # print(body.data.payload.text)
+    # print('------------------------------------------------------------------------')
+    # print(body)
+    return 'success', 200
+    
 if __name__ =="__main__":
     app.run(port=5000)
